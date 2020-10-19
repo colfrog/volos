4357a550c654a16bb3c42ff4507d6ab9112b54b1
Laurent Cimon <laurent@nilio.ca>
2020-10-19 12:09:52 -0400

    Ajout d'un étape babel au système de compilation

---
+++ Volos/src/main/resources/build-jsx.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Il faut installer node-babel-cli et node-babel-preset-react
+# sudo apt install node-babel-cli node-babel-preset-react
+babeljs src/main/resources/jsx --out-dir src/main/webapp/components --presets react
