a99e6c843522a809e5d9582a870f17f65b565d2b
Paul <pauldrdlg@gmail.com>
2020-10-19 12:17:41 -0400

    update script BD commune

--- Scripts BD/Destruction tables.sql
+++ Scripts BD/Destruction tables.sql
@@ -1,7 +1,11 @@
 ---------- Destruction des tables, des données, du TRIGGER et de la fonction ----------
 
-DROP TRIGGER update_evenement ON annonce;
-DROP FUNCTION insert_evenement();
+DROP TRIGGER insert_evenement_trigger ON annonce;
+DROP TRIGGER update_evenement_trigger ON annonce;
+DROP TRIGGER delete_evenement_trigger ON annonce;
+DROP FUNCTION insert_evenement_function();
+DROP FUNCTION update_evenement_function();
+DROP FUNCTION delete_evenement_function();
 
 DELETE FROM auteur_livre;
 DELETE FROM auteur;
