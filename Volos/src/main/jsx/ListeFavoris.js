142b6208303389683a53d6daac6b4ec48ae3a640
Laurent Cimon <laurent@nilio.ca>
2020-11-05 11:08:47 -0500

    Réécrit ListeFavoris avec ListeAnnonces

--- Volos/src/main/jsx/ListeFavoris.js
+++ Volos/src/main/jsx/ListeFavoris.js
@@ -1,44 +1,4 @@
-class Favori extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cip: this.props.cip,
-            id: this.props.id,
-            titre: this.props.titre,
-            description: this.props.description,
-            prix: this.props.prix,
-            estFavori: this.props.estFavori
-        };
-    }
-
-    ajouter() {
-        this.setState({estFavori: true});
-        fetch(`/Volos/api/ajouter_favori?cip=${this.state.cip}&id=${this.state.id}`);
-    }
-
-    retirer() {
-        this.setState({estFavori: false});
-        fetch(`/Volos/api/retirer_favori?cip=${this.state.cip}&id=${this.state.id}`);
-    }
-
-    render() {
-        let bouton = null;
-        if (this.state.estFavori)
-            bouton = <button onClick={this.retirer.bind(this)}>Retirer des favoris</button>;
-        else
-            bouton = <button onClick={this.ajouter.bind(this)}>Ajouter aux favoris</button>;
-
-        return (
-            <div className="card">
-                <img src="https://i.imgur.com/gPEswtC.jpg" />
-                <p>{this.state.titre}</p>
-                <p>{this.state.description}</p>
-                <p className="price">{this.state.prix}</p>
-                {bouton}
-            </div>
-        );
-    }
-}
+import ListeAnnonces from '/Volos/components/ListeAnnonces.js';
 
 class ListeFavoris extends React.Component {
     constructor(props) {
@@ -85,4 +45,12 @@ class ListeFavoris extends React.Component {
 }
 
 var domContainer = document.querySelector('#liste_favoris');
-ReactDOM.render(<ListeFavoris/>, domContainer);
+ReactDOM.render(<ListeAnnonces fetcher={(callback) => {
+    fetch('/Volos/api/loggedUtilisateur')
+        .then(data => data.json())
+        .then(utilisateur => {
+            fetch('/Volos/api/favoris?cip='+utilisateur.cip)
+                .then(data => data.json())
+                .then(callback);
+        });
+}}/>, domContainer);
