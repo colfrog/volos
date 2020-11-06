4bb1c7591a3ba5389178fbaa3721f30add46b20f
Laurent Cimon <laurent@nilio.ca>
2020-11-06 13:06:26 -0500

    Ajoute un service de photos pour les annonces

--- Volos/src/main/jsx/Annonce.js
+++ Volos/src/main/jsx/Annonce.js
@@ -10,6 +10,7 @@ export default class Annonce extends React.Component {
             prix: this.props.prix,
             dateAffichage: this.props.dateAffichage,
             categorie: this.props.categorie,
+            hasPhoto: false,
             titre: this.props.titre,
             resume: this.props.resume,
             maisonEdition: this.props.maisonEdition,
@@ -38,7 +39,7 @@ export default class Annonce extends React.Component {
                     nom: utilisateur.nom,
                     prenom: utilisateur.prenom
                 })
-            })
+            });
 
         fetch('/Volos/api/showPublishAnnonce?id='+urlParams.get('id'))
             .then(data => data.json())
@@ -53,7 +54,6 @@ export default class Annonce extends React.Component {
                         />)
                     });
                 }
-
 
                 let dateAffichage = "";
                 let datePublication = "";
@@ -81,6 +81,9 @@ export default class Annonce extends React.Component {
                         + "-" + dateFin.getFullYear();
                 }
 
+                fetch(`/Volos/api/hasPhoto?id=${annonce.id}`)
+                    .then(data => data.json())
+                    .then(hasPhoto => this.setState({hasPhoto: hasPhoto}));
 
                 this.setState({
                     mail: annonce.cip + "@usherbrooke.ca",
@@ -102,7 +105,10 @@ export default class Annonce extends React.Component {
 
     render() {
         let resume = "", maisonEdition = "", datePublication = "", nbChambre = "",
-            dateDebutLocation = "", dateFinLocation = "", auteurs = "", image="autre.jpg";
+            dateDebutLocation = "", dateFinLocation = "", auteurs = "", image = "autre.jpg";
+
+        if (this.state.hasPhoto)
+            image = `/Volos/api/photo?id=${this.state.id}`;
 
         if(this.state.categorie === "LIVRE")
         {
@@ -120,14 +126,15 @@ export default class Annonce extends React.Component {
                 auteurs = <div>Auteur: {listeAuteurs}</div>
             }
 
-            image="livre.jpg"
+            image = "livre.jpg";
         }
         else if(this.state.categorie === "LOYER")
         {
             nbChambre = <p>Nombre de chambres: {this.state.nbChambre}</p>
             dateDebutLocation = <p>Date de début de location: {this.state.dateDebutLocation}</p>
             dateFinLocation = <p>Date de fin de location: {this.state.dateFinLocation}</p>
-            image="loyer.jpg"
+
+            image = "loyer.jpg";
         }
 
         var subjectMail = "Offre Volos";
